3cca82d8450f8404fdea50bc1d396000a1574109
Радченко Андрей Михайлович <a.radchenko@watcom.ru>
2020-12-08 12:09:14 +0300

    correct beta fix

--- nodejs/hhru/index.js
+++ nodejs/hhru/index.js
@@ -11,6 +11,7 @@ await db.clusters.dropTable()
 await db.clusters.createTable()
 
 var found_pages
+var vacancy_ids_list = []
 
 // get clusters from hh.ru
 await Promise.all([api.getClusters()])
@@ -33,42 +34,36 @@ await Promise.all([api.getClusters()])
     console.log(err)
   })
 
-// get vacancies & save into db
+// get vacancies ids
 for(var count = 0; count < found_pages; count++) {
-  await new Promise(resolve => setTimeout(resolve, 5000)) // wait 5 secs (ddos guard)
   await Promise.all([api.getVacancies(count)])
-    .then((r) => {
-      // vacancies save into db
-      for(const v of r[0]) {
-        Promise.all([api.getVacancy(v.id)])
-          .then((r) => {
-            const vacancy = r[0]
-            db.vacancies.add({
-              id: vacancy.id,
-              name: vacancy.name, 
-              area: vacancy.area, 
-              salary: vacancy.salary, 
-              type: vacancy.type, 
-              experience: vacancy.experience,
-              schedule: vacancy.schedule,
-              employment: vacancy.employment,
-              description: vacancy.description,
-              key_skills: vacancy.key_skills,
-              employer: vacancy.employer, 
-              published_at: vacancy.published_at, 
-              created_at: moment(new Date()).utc()
-            })
-            console.log(`INFO: Vacancy id: ${vacancy.id}, name: "${vacancy.name}" saved.`)
-          }) // .then vacancy
-          .catch(err => {
-            console.log(err)
-          }) // .catch
-      } // for v
-    }) // .then vacancies
-    .catch(err => {
-      console.log(err)
-    })
-    .finally(
-      console.log('That`s all folks...')
-    )
+  .then((r) => {
+    for (const v of r[0]) {
+      vacancy_ids_list.push(v.id)
+    }    
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+// vacancies save into db
+for(const id of vacancy_ids_list) {
+  const vacancy = await api.getVacancy(id)
+  db.vacancies.add({
+    id: vacancy.id,
+    name: vacancy.name, 
+    area: vacancy.area, 
+    salary: vacancy.salary, 
+    type: vacancy.type, 
+    experience: vacancy.experience,
+    schedule: vacancy.schedule,
+    employment: vacancy.employment,
+    description: vacancy.description,
+    key_skills: vacancy.key_skills,
+    employer: vacancy.employer, 
+    published_at: vacancy.published_at, 
+    created_at: moment(new Date()).utc()
+  })
+  console.log(`INFO: Vacancy id: ${vacancy.id}, name: "${vacancy.name}`)
 }
